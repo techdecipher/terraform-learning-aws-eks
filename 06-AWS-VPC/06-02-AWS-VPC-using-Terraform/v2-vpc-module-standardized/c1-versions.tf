f35cbe27794efbc137f880cc0d11b84cc7e60942
techdecipher <waytopranav22@gmail.com>
2026-06-03 21:33:26 +0530

    Added files for v2 vpc

---
+++ 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/v2-vpc-module-standardized/c1-versions.tf
@@ -0,0 +1,19 @@
+#Terraform block
+terraform {
+  required_version = "~> v1.15.1"  // ~> allows right most will not use other version like v1.16xx because currently the right most version is v1.15 at the time of this development
+  //~> means version locking
+
+  required_providers {
+    aws = {
+       # goto https://registry.terraform.io/providers/hashicorp/aws/latest and copy the source and versions 
+       source  = "hashicorp/aws"
+       version = "6.44.0"
+    }
+  }
+}
+
+#Provider Block
+provider "aws" {
+
+    region = var.aws_region
+}
---
+++ 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/v2-vpc-module-standardized/terraform.tfvars
@@ -0,0 +1,6 @@
+
+# Auto load values from tfvars to your variables file
+
+aws_region = "us-east-1"
+environment = "Dev"
+business_division = "SAP"
